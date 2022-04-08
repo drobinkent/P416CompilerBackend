2f5e88274c11c5b356bcea3ef06a89075b666ecc
Debobroto Das Robin <drobin@rani.medianet.cs.kent.edu>
2022-04-08 14:33:35 -0400

    Added support for phb waste calculation

--- src/main.py
+++ src/main.py
@@ -8,27 +8,29 @@ from ParserMapper import HeaderLib
 from ParserMapper import *
 from ParserMapper import RefCount
 from P416JsonParser import  ParserOpOp
+from utils import Util
 
 #64, 96, and 64 words of 8, 16, and 32b
 p4ProgramParserFactory = P4ProgramParserFactory()
 hw = RMTHardwareFactory.createRmtHardware(rmtHardwaRemodelName = "RMT_V1",
     instructionSetConfigurationJsonFile= "../Resources/HardwareConfigs/RMTV1ModelInstructions.json",
     hardwareSpecConfigurationJsonFile = "../Resources/HardwareConfigs/RMTV1model32Stages.json")
-p4program = p4ProgramParserFactory.getParsedP4Program(p4JsonFile="../Resources/curated_programs_for_paper/l2l3_nsdi/l2l3_complex.json",p4VersionAndArchitecture="P416_V1_Model")
+p4program = p4ProgramParserFactory.getParsedP4Program(p4JsonFile="../Resources/P4ProgramsForPaper/L2L3Complex/l2l3complex.json",p4VersionAndArchitecture="P416_V1_Model")
 p4program.buildHeaderVector(hw)
 p4ProgramGraph = P4ProgramGraph(p4program)
 
-headerFieldSpecsInP4ProgramToBeUsedForParserMapper = p4ProgramGraph.headeranalyzer(hw)
+headerFieldSpecsInP4ProgramToBeUsedForParserMapper, totalRawBitwidth = p4ProgramGraph.headeranalyzer(hw)
 parseGraphHeaderList, parsedGraphHeaders, initHeader = HeaderLib.loadParseGraph(parserObject = p4program.parsers[0], p4ProgramGraph = p4ProgramGraph) # There is only one parser in v1model
 buildParserMapper(parseGraphHeaderList, parsedGraphHeaders, hw, initHeader)
 
 p4ProgramGraph.loadPipelines(hw)
-headerFieldSpecsInP4Program = p4ProgramGraph.headeranalyzer(hw)
+headerFieldSpecsInP4Program,totalRawBitwidth = p4ProgramGraph.headeranalyzer(hw)
 # mappedPacketHeaderVector1 = hw.mapHeaderFieldsUsingGoogleOR(headerFieldSpecsInP4Program)
 # headerFieldSpecsInP4Program = {33: 1, 16: 1} # this line is for test purpose
 mappedPacketHeaderVector = hw.mapHeaderFields(headerFieldSpecsInP4Program)
 print(mappedPacketHeaderVector)
 p4ProgramGraph.storePHVFieldMappingForHeaderFields(mappedPacketHeaderVector=mappedPacketHeaderVector)
+Util.calculatePHVWaste(headerFieldSpecsInP4Program, mappedPacketHeaderVector,totalRawBitwidth)
 p4ProgramGraph.embedPipelines(hw)
 hw.calculateTotalLatency(p4ProgramGraph, hw)
 
